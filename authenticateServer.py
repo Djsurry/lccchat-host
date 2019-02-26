f57806d19e7aa8dbf845a9dea5effb037f447e18
David Surry <djsurry@gmail.com>
2019-02-26 10:29:39 -0500

    gix ;

--- authenticateServer.py
+++ authenticateServer.py
@@ -24,7 +24,7 @@ def verify(email, pubkey):
     c = conn.cursor()
     e = [n for n in c.execute("select email from users where email=?", (hash_string(email),))]
     if not e:
-        sys.stdout.write("MAKING NEW ENTRY")
+        write("MAKING NEW ENTRY")
         hash = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=15))
         c.execute("insert into users values (?, ?, ?, ?, ?)", (hash_string(email), pubkey, "0 ", "", hash))
         conn.commit()
@@ -37,7 +37,7 @@ def verify(email, pubkey):
     hashes = users[1].split(' ')
     if pubkey in pubkeys:
         conn.close()
-        sys.stdout.write(213123123123121)
+        write(213123123123121)
         return "https://lccchat.me/verify?token={}".format(hashes[pubkeys.index(pubkey)])
     pubkeys.append(pubkey)
     hashes.append(''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=15)))
@@ -61,48 +61,47 @@ def auth(host):
     c = conn.cursor()
     print('starting auth process')
     email = host.socket.read(blocking=True)
-    sys.stdout.write("1")
-    sys.stdout.flush()
+    write("1")
+
     pubkey = host.socket.read(blocking=True)
-    sys.stdout.write('2')
-    sys.stdout.flush()
+    write('2')
+
     # if not email or not pubkey:
     #     sys.stdout.write("EMAIL OR PUBKEY NOT SENT")
     #     return False
     
     a = [n for n in c.execute("select email from users where email=?", (hash_string(email),))]
-    sys.stdout.write('3')
-    sys.stdout.flush()
+    write('3')
+
 
     if not a:
         write('here')
         sendEmail(email, "Verify", "Click here: {}".format(verify(email, pubkey)))
         host.socket.send("ERR VERIFY {}".format(email))
-        sys.stdout.write("NOT ON RECORD")
+        write("NOT ON RECORD")
         return False
     write(str([n for n in c.execute("select pubkey, verified from users where email=?", (hash_string(email),))][0]))
     r = [n for n in c.execute("select pubkey, verified from users where email=?", (hash_string(email),))][0]
-    sys.stdout.write(f"r1: {r}")
-    sys.stdout.write('5')
-    sys.stdout.flush()
+    write(f"r1: {r}")
+    write('5')
     p = [n for n in r[0].split(":") if n]
-    sys.stdout.write('6')
-    sys.stdout.write(f"r1: {r[1]}")
-    sys.stdout.write(f"p: {p}")
-    sys.stdout.flush()
+    write('6')
+    write(f"r1: {r[1]}")
+    write(f"p: {p}")
+
     a = [int(n) for n in r[1].split()]
 
-    sys.stdout.write('7')
-    sys.stdout.flush()
+    write('7')
+
     if pubkey in p:
-        sys.stdout.write("ASDAS")
-        sys.stdout.write(f"a: {a}")
-        sys.stdout.write(f"index: {p.index(pubkey)}")
-        sys.stdout.flush()
+        write("ASDAS")
+        write(f"a: {a}")
+        write(f"index: {p.index(pubkey)}")
+
 
         if a[p.index(pubkey)] == 0:
-            sys.stdout.write("VERIFY NEEDED")
-            sys.stdout.flush()
+            write("VERIFY NEEDED")
+
             sendEmail(email, "Verify", "Click here: {}".format(verify(email, pubkey)))
             host.socket.send("ERR VERIFY {}".format(email))
             return False
@@ -120,11 +119,11 @@ def auth(host):
             return key, email
     
     else:
-        sys.stdout.write("3123")
-        sys.stdout.flush()
+        write("3123")
+
         sendEmail(email, "Verify", "Click here: {}".format(verify(email, pubkey)))
         host.socket.send("ERR VERIFY {}".format(email))
-        sys.stdout.write("PUBKEY NOT IN LISTED PUBKEYS")
-        sys.stdout.flush()
+        write("PUBKEY NOT IN LISTED PUBKEYS")
+
         return False
     
